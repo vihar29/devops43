6bb0a43a11d3d92ecfc353edb3b3d82e3c75a416
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-08 17:29:20 -0500

    Update novars.sh

--- novars.sh
+++ novars.sh
@@ -1,3 +1,6 @@
 #!bin /bash
 echo "welcone to canada"
 echo -e "city names are\nwindsor\ntoronto\tmontreal"
+echo -e "windsor has a caesars"
+echo -e "windsor has a lot of opportunities"
+echo -e "windsor has university of windsor/twindsor has 22.5km coastal area"
