7f80238e8a966c3d1090d4db6424b18a30e63fa8
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-08 18:16:25 -0500

    Create svars.sh

---
+++ svars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+## input should be provided as arguments
+## ex: script input1 input2
+## by using special variables in shell we can get the arguments parsed into script
+## spec vars are 0 to n,@,*,# .
+ech0 $0
+echo $1= first arguments
+echo $2= second arguments
+echo $@
