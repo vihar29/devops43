9e715af6f8564aca82216331952026471d10bef1
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-08 17:34:41 -0500

    Create vars.sh

---
+++ vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+## declaring a variable 
+## syntax ::VAR_NAME=DATA
+## no data types in shell scripting by default , means everything is a string
+a=10
+x=abc
+## how to access a variable
+## $(VAR_NAME) or $VAR-NAME
+echo $a
