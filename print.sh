ec2a580adaf58502c8290f52aa00d8738c09f598
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-08 16:50:28 -0500

    Update print.sh

--- print.sh
+++ print.sh
@@ -7,3 +7,5 @@ echo line1/n line2
 ## to enable escape sequences then you have to -e option
 echo -e line1/n line2
 ## -e enables escape n to be used as a new line
+## always use double quotes when we enable escape sequence
+echo -e "line1/n line2"
