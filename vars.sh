06c4779a08bde6a167e0c425c87b7642b79476ab
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-08 17:51:42 -0500

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -9,3 +9,12 @@ x=abc
 echo $a
 echo "course name =$course_name"
 
+## use export command to use environmental variables
+DATE=2019-12-08
+echo -e "hello good evng ,today date is $DATE"
+## above statement is true for one day if the date has changed
+## if same scripts need to be running daily 
+## to get info dynamically execuiting commands.
+## command substitution syntax :: VAR_NAME=$(commands)
+DATE=$(date +%F)
+echo -e "hello good evening,today date is$DATE"
