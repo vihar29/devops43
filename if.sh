c8a96a59ba0cd878ae95c4e8184290641d568019
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-08 21:42:53 -0500

    Create if.sh

---
+++ if.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+## if usuallly seem in three forms
+## simple if
+# if [expression];then
+
+#commands
+#fi
+## commands will be executed if expression is true
+
+## if else
+# if [expression];then
+#commandsA
+#else
+#commandb
+#fi
+## commandsA will be executed if expression is true, else if expression is false then commandsB will be executed
+## expressions
+#1. string checks
+#["abc"="ABC"]
+#operators : =,==,!=,-z.
+# 2.number checks
+#operators : -eq, -ne, -gt ,-lt ,-le ,-ge
+#3. file checks
+# refer , man bash to get all the file related operations
+action =$1
+if ["$action" == "start"]; 
+then 
+echo "starting service"
+exit 0
+elif ["$action"=="stop"];
+then 
+echo "stopping service"
+exit 0
+ 
