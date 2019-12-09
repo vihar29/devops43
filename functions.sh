46561578fd56f7c78fc828bbfe448a8be25e43b6
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-08 19:44:26 -0500

    Create functions.sh

---
+++ functions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## if we assign a name to a set of commands then it is a function
+
+## declare a function
+## example1 : showing syntax1
+function sample(){
+    echo "iam a sample function"
+}
+##example2 :showing syntax2
+function sample2 {
+    echo "iam a sampke1  function"
+}
+##
+sample
+sample2
