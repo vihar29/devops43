2d2833df720fd02aa2fed8f17857619ae52f749d
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-09 18:43:39 -0500

    Update web.sh

--- project/web.sh
+++ project/web.sh
@@ -3,15 +3,27 @@
 LOG=/tmp/stack.log
 
 
-
-
-
+#functions
+STATUS_CHECK()
+{
+    if [$1 -eq 0];then
+    echo " $2 --  SUCCESS"
+    else
+    echo " $2 --  FAILURE"
+    exit 1
+    fi
+}
 
 #web server installation
 yum install nginx -y &>>$LOG
-if [ $? -eq 0 ];then
-echo "nginx installation success"
-else 
-echo "nginx installation failure"
-exit 1
-fi
+STATUS_CHECK $? "NGINX SERVER INSTALLATION"
+
+rm -rf /usr/share/nginx/html/*  &>>$LOG
+STATUS_CHECK $? "REMOVE OLD WEBAPPS CONTENT"
+
+#curl -s https://devopsb43n.s3.amazonaws.com/d43.zip -o /tmp/d43.zip 
+#unzip /tmp/d43.zip -d "/usr/share/nginx/html/"
+
+## variable declared in function can be accessed and modified inside function
+## a variable declared in function main program can be accessed and modified inside function
+
