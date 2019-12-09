2b1f9f29f65c69f8d53bf78299e1ad5799e3e848
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-09 18:28:29 -0500

    Update web.sh

--- project/web.sh
+++ project/web.sh
@@ -9,8 +9,7 @@ LOG=/tmp/stack.log
 
 #web server installation
 yum install nginx -y &>>$LOG
-if [ $? -eq 0 ]:
-then
+if [ $? -eq 0 ];then
 echo "nginx installation success"
 else 
 echo "nginx installation failure"
