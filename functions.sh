c3640fd8277de7b1a10c844fd9da5324be209be6
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-08 20:00:13 -0500

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -18,3 +18,14 @@ Demo1() {
     echo "first argument =$1"
 }
 Demo1 "abc"
+## function is a command so it should have a exit status
+## we cannot use exit command inside function it will exit the entire script
+## we use return command to come out of function
+## return status also has same status as exit status from 0-255
+demo2(){
+    echo "windsor is beautiful"
+    return 0
+    echo "harsha goud"
+
+}
+demo2
