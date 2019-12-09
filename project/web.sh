b39e14b4a0816832dea4259626414e9f56bf4f7b
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-09 18:56:10 -0500

    Update web.sh

--- project/web.sh
+++ project/web.sh
@@ -12,9 +12,9 @@ N="\e[0m"
 STATUS_CHECK()
 {
     if [ $1 -eq 0 ];then
-    echo - e " $2 --  ${G}SUCCESS${N}"
+    echo -e "$2 --  ${G}SUCCESS${N}"
     else
-    echo -e " $2 --  ${R}FAILURE${N}"
+    echo -e "$2 --  ${R}FAILURE${N}"
     exit 1
     fi
 }
