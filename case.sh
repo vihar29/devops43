c1135314c482e9f03fc18fff4e08f5446e67ee34
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-08 20:16:00 -0500

    Create case.sh

---
+++ case.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+##case $var in
+#pattern1)
+#cmd
+#cmd
+#;;
+#pattern2)
+#cmd
+#cmd
+#;;
+#esac
+Action=$1
+case $action in
+start)
+echo "starting service"
+exit 0
+;;
+stop)
+echo "stopping service"
+exit 0
+;;
+echo "invalid input"
+exit 1
+;;
+esac
