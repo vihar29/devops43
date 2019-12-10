592c83c32674030e63e224b162bb1263fe18e75d
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-09 20:30:34 -0500

    Update web.sh

--- project/web.sh
+++ project/web.sh
@@ -4,6 +4,8 @@ LOG=/tmp/stack.log
 G="\e[32m"
 R="\e[31m"
 N="\e[0m"
+APPUSER=student
+TOMCAT_VERSION=8.5.49
 
 
 
@@ -43,6 +45,44 @@ STATUS_CHECK $? "UPDATE CONFIGURATION FILE"
 systemctl enable nginx &>>$LOG
 systemctl restart nginx &>>$LOG
 STATUS_CHECK $? "STARTING NGINX SERVER"
+
+#app server setup
+HEAD "APP SERVER SETUP"
+yum install java -y &>>$LOG
+STATUS_CHECK $? "JAVA IBSTALLATION SUCCESS"
+id $APPUSER &>>$LOG 
+if [ $? -ne 0 ]; then 
+  useradd $APPUSER 
+  STATUS_CHECK $? "Add Application User"
+fi 
+
+cd /home/$APPUSER
+curl -s https://archive.apache.org/dist/tomcat/tomcat-8/v{TOMCAT_VERSION}/bin/apache-tomcat-{TOMCAT_VERSION}.tar.gz | tar -xz
+STATUS_CHECK $? "download tomcat"
+cd /home/$APPUSER/apache-tomcat-${TOMCAT_VERSION} 
+curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/student.war -o webapps/student.war
+STATUS_CHECK $? "Download StudentApp Application"
+
+cd /home/$APPUSER/apache-tomcat-${TOMCAT_VERSION} 
+curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/mysql-connector.jar -o lib/mysql-connector.jar
+STATUS_CHECK $? "Download MySQL JDBC Driver"
+
+sed -i -e '/TestDB/ d' -e '/\/Context/ i <Resource name="jdbc/TestDB" auth="Container" type="javax.sql.DataSource" maxTotal="100" maxIdle="30" maxWaitMillis="10000" username="student" password="student1" driverClassName="com.mysql.jdbc.Driver" url="jdbc:mysql://{DBSERVER-IPADDRESS}:3306/studentapp"/>' conf/context.xml 
+STATUS_CHECK $? "Add JDBC Configuration"
+
+chown $APPUSER:$APPUSER /home/$APPUSER -R 
+
+curl -s -o /etc/init.d/tomcat https://s3-us-west-2.amazonaws.com/studentapi-cit/tomcat-init 
+chmod +x /etc/init.d/tomcat
+STATUS_CHECK $? "Download Tomcat INIT Script"
+
+systemctl daemon-reload &>>$LOG 
+STATUS_CHECK $? "Reload Systemd Daemon"
+
+systemctl restart tomcat
+STATUS_CHECK $? "Start Tomcat Service"
+
+
 
 
 
