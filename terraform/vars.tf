b79d87cf442055fc3b7a874840889ed1a8be73e2
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-16 18:58:01 -0500

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,7 +2,7 @@
 
 
 variable "simple-string" {
-  type = "string" // no need of this if you give the value in double quotes
+  type = string // no need of this if you give the value in double quotes
   default = "SIMPLE"
 }
 // Note : if you give the value in double quotes tf automatically considers  it as a string
@@ -15,7 +15,7 @@ variable "simple-boolean" {
   type = bool // no need of giving them in double quotes if defaut value is boolean
   default = true
 }
-resource "null_resource" "sample" {
+resource "null_resource" "sample2" {
   provisioner "local-exec" {
     command = "echo -e \" string = ${var.simple-string}\n boolean = ${var.simple-boolean}\n number =${var.simple-number}\""
   }
