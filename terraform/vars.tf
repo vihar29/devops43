a8e8a9c619b7dac29dd3c5021a917e39564c85a5
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-16 18:50:31 -0500

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,23 @@
+// Declare a string
+
+
+variable "simple-string" {
+  type = "string" // no need of this if you give the value in double quotes
+  default = "SIMPLE"
+}
+// Note : if you give the value in double quotes tf automatically considers  it as a string
+
+variable "simple-number" {
+  type = number // no need of giving them in double quotes if defaut value is number
+  default = 100
+}
+variable "simple-boolean" {
+  type = bool // no need of giving them in double quotes if defaut value is boolean
+  default = true
+}
+resource "null_resource" "sample" {
+  provisioner "local-exec" {
+    command = "echo -e \" string = ${var.simple-string}\n boolean = ${var.simple-boolean}
+  }
+}
+
