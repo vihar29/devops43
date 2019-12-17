e4f4c194a98de6999c3b66898606a9352f7d13bf
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-16 22:52:56 -0500

    Update project.tf

--- terraform/project.tf
+++ terraform/project.tf
@@ -10,6 +10,10 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0f9f5f95328faead3"]
   provisioner "remote-exec" {
+    connection {
+      user = "root"
+      password = "DevOps321"
+    }
     inline = [
     "yum install nginx -y"
     ]
