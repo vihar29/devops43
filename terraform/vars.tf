0d5c413da3dbb0ef44cb8cf3bd880c445dca0395
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-16 19:09:28 -0500

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -15,9 +15,17 @@ variable "simple-boolean" {
   type = bool // no need of giving them in double quotes if defaut value is boolean
   default = true
 }
+variable "simple-list" {
+  type = "list"
+  default = [1 ,2]
+
+}
 resource "null_resource" "sample2" {
   provisioner "local-exec" {
     command = "echo -e \" string = ${var.simple-string}\n boolean = ${var.simple-boolean}\n number =${var.simple-number}\""
+  }
+  provisioner "local-exec" {
+    command = "echo -e value1 = ${var.simple-list a[1] }"
   }
 }
 
