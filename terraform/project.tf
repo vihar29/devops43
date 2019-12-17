331d54c42eda6dfa9be4608ccd39cc4f4f89f740
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-16 23:36:51 -0500

    Update project.tf

--- terraform/project.tf
+++ terraform/project.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "web" {
     }
     inline = [
       "cd /tmp" ,
-      "git clone https://Vihar29:Vih@r100121@github.com/vihar29/devops43.git",
+      "git clone https://github.com/vihar29/devops43.git",
       "cd project/",
       "sh web.sh"
     ]
