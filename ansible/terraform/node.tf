083c87fb38a83f77b769e4eb16e0fa3996d74734
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-17 23:42:46 -0500

    Update node.tf

--- ansible/terraform/node.tf
+++ ansible/terraform/node.tf
@@ -23,6 +23,10 @@ resource "aws_instance" "node2" {
   }
 
 }
+resource "local_file" "file" {
+  filename = "../hosts-inventory"
+  content = "${aws_instance.node1.public_ip}\n${aws_instance.node2.public_ip\}"
+}
 
 
 
