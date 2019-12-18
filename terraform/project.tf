0e43c5279b751dbaa76b5aaa169ec06770d66e35
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-17 20:52:38 -0500

    Update project.tf

--- terraform/project.tf
+++ terraform/project.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-west-2"
 }
 variable "aws_ami" {
-default = "ami-02c419e998e13412a"
+default = "ami-0c98a6bf9be7285b5"
 }
 
 resource "aws_instance" "web" {
   ami = var.aws_ami
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0f9f5f95328faead3"]
+  vpc_security_group_ids = ["sg-0f9f5f95328faead3","sg-0c1e7ed2b4feb7930","sg-08d1de278ab886323"]
   provisioner "remote-exec" {
     connection {
       host = self.public_ip
