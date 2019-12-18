bd6c8aaabcf7e531e225bc2793d990219181ef42
vihar29 <58472893+vihar29@users.noreply.github.com>
2019-12-17 22:30:22 -0500

    Create node.tf

---
+++ ansible/terraform/node.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-west-2"
+}
+variable "aws_ami" {
+  default = "ami-0c98a6bf9be7285b5"
+}
+
+resource "aws_instance" "node1" {
+  ami = var.aws_ami
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0f9f5f95328faead3"]
+  tags {
+    Name ="NODE1"
+  }
+
+}
+resource "aws_instance" "node2" {
+  ami = var.aws_ami
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0f9f5f95328faead3"]
+  tags {
+    Name ="NODE2"
+  }
+
+}
+
+
+
