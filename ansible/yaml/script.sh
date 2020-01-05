dcb8d62eccf9ba169b4ee1156ddc23f64925e6b1
vihar29 <58472893+vihar29@users.noreply.github.com>
2020-01-05 14:21:02 -0500

    Update script.sh

--- ansible/yaml/script.sh
+++ ansible/yaml/script.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+
+
 if [ ! -f "$1" ];then
 echo "given file does not exist"
 exit 1
 fi
 
-echo -e "hello abcd,\n\t my name is harsha"
+i=0
+STUDENT_COUNT=$(cat batch43.yml | yq . | jq '.studentDetails[].name' | wc -1)
+NAMES=('cat batch43.yml | yq . | jq '.studentDetails[].name'|xargs')
+TRAINER_NAME=$(cat batch43.yml | yq . | jq '.trainerName')
+
+ 
+ while [$STUDENT_COUNT -gt 0]; do
+
+ echo -e "hello ${NAMES[$1]},\n\tmy name is $($TRAINER_NAME) "
+ STUDENT_COUNT=$(($STUDENT_COUNT-1))
+ i=$(($i+1))
+ done
+ 
