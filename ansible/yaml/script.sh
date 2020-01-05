4d29f6d41ebaf19ea279d7305c6c36016f511f8b
vihar29 <58472893+vihar29@users.noreply.github.com>
2020-01-05 14:31:47 -0500

    Update script.sh

--- ansible/yaml/script.sh
+++ ansible/yaml/script.sh
@@ -12,7 +12,7 @@ NAMES=('cat batch43.yml | yq . | jq '.studentDetails[].name'|xargs')
 TRAINER_NAME=$(cat batch43.yml | yq . | jq '.trainerName')
 
  
- while [$STUDENT_COUNT -gt 0]; do
+ while [ $STUDENT_COUNT -gt 0 ]; do
 
  echo -e "hello ${NAMES[$1]},\n\tmy name is $($TRAINER_NAME) "
  STUDENT_COUNT=$(($STUDENT_COUNT-1))
