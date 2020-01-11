ecbee32ac95c140219600c75dd0f38e619393480
vihar29 <58472893+vihar29@users.noreply.github.com>
2020-01-10 20:55:41 -0500

    Create novars.sh

---
+++ shellscripts/novars.sh
@@ -0,0 +1,7 @@
+#!bin /bash
+echo "welcone to canada"
+echo -e "city names are\nwindsor\ntoronto\tmontreal"
+echo -e "windsor has a caesars"
+echo -e "windsor has a lot of opportunities"
+echo -e "windsor has university of windsor/twindsor has 22.5km coastal area"
+## u need to use backward slash to work with escape sequences
