6ad6e33d23ce8ead39688b4276bbd27d5baec4d8
vihar29 <58472893+vihar29@users.noreply.github.com>
2020-01-12 16:03:33 -0500

    Update node.tf

--- ansible/terraform/node.tf
+++ ansible/terraform/node.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "node1" {
   ami = var.aws_ami
   instance_type = "t2.micro"
   key_name = "yourkey"
-  vpc_security_group_ids = ["sg-0f9f5f95328faead3"]
+  vpc_security_group_ids = ["sg-0f9f5f95328faead3","sg-0c1e7ed2b4feb7930","sg-08d1de278ab886323"]
   iam_instance_profile = "EC2-READONLY"
   tags = {
     Name ="NODE1"
